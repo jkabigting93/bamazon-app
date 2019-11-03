944333ddc10dcc5f7eeaa535d5f3364f0805a9cb
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-03 16:44:27 -0500

    separated queryInventory and promptUser functions to be able to call them separately

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,36 +23,39 @@ connection.connect(function(err) {
     queryInventory();
 });
   
-// Define queryInventory function
+// Define queryInventory function - logs all available items
 function queryInventory() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-
-// Logs all available items to console
         console.log("Item ID | Product Name | Department Name | Price ($) | Stock Quantity");
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
         console.log("---------------------------------------------------------");
-        inquirer.prompt([
-
-// Use inquirer to ask users what they wish to buy
-            {
-                type: "input",
-                name: "id",
-                message: "Please provide the Item ID of the product you wish to purchase:"
-            },
-
-// Next, ask how much of that item they would like to purchase
-            {
-                type: "input",
-                name: "quantity",
-                message: "How much of this item would you like to purchase?"
-            }
-
-// Check if Bamazon has enough supply of that particular item
-        ]).then(function(response) {
-            console.log(response);
-        })
-    });
+        promptUser();
+    })
 };
+
+// Define promptUser function - questions to determine what to buy, and updates db accordingly
+function promptUser() {
+    inquirer.prompt([
+        
+        // Use inquirer to ask users what they wish to buy
+        {
+            type: "input",
+            name: "item",
+            message: "Please provide the Item ID of the product you wish to purchase:"
+        },
+        
+        // Next, ask how much of that item they would like to purchase
+        {
+            type: "input",
+            name: "quantity",
+            message: "How much of this item would you like to purchase?"
+        }
+        
+        // Check if Bamazon has enough supply of that particular item
+    ]).then(function(response) {
+        console.log(response);
+    })
+}
