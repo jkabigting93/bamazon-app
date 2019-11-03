ee9815e79ddb661293a148cfebd34994dee9c335
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-03 15:46:08 -0500

    added gitignore, required mysql, and console logged available items on node app start

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,32 @@
-console.log(
-    "************************************************************",
-    "\nWelcome to Bamazon!",
-    "\nThe following produts are available for purchase today:",
-)
+// Initialize MySQL
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mArch1993!",
+    database: "bamazon"
+});
+
+// Connect to Bamazon MySQL database and call queryInventory function
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(
+        "************************************************************",
+        "\nWelcome to Bamazon!",
+        "\nThe following produts are available for purchase today:"
+    );
+    queryInventory();
+});
+  
+// Define queryInventory function (shows all available items on app start)
+function queryInventory() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("---------------------------------------------------------");
+    });
+  }
