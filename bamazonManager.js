231e05e93e780a50aed707eb104373b27aece234
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-03 20:12:12 -0500

    added low count function

--- bamazonManager.js
+++ bamazonManager.js
@@ -39,13 +39,25 @@ function managerActions() {
                     console.log("Item ID | Product Name | Price ($) | Stock Quantity");
                     for (var i = 0; i < res.length; i++) {
                         console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity)
-                    }
+                    };
                     console.log("---------------------------------------------------------");
                     managerRestart();
                 })
                 break;
             case "View Low Inventory":
-
+                connection.query("SELECT * FROM products where stock_quantity < 5", function(err,res) {
+                    if (err) throw err;
+                    console.log(
+                        "The following items have fewer than 5 units left:",
+                        "\n",
+                        "\nItem ID | Product Name"
+                    );
+                    for  (var i = 0; i < res.length; i++) {
+                        console.log(res[i].item_id + " | " + res[i].product_name)
+                    };
+                    console.log("---------------------------------------------------------");
+                    managerRestart();
+                })
                 break;
             case "Add to Inventory":
                 break;
